2c8d3b618252a171b7349694f8d10c46e84e0262
raniacode-cmd <raniashahin209@gmail.com>
2026-08-02 13:08:49 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,40 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { createBrowserRouter , RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './componant/Layout/Layout';
-import Home  from "./componant/Home/Home";
-import Blog  from "./componant/Blog/Blog";
- 
+import Home from "./componant/Home/Home";
+import Blog from "./componant/Blog/Blog";
+
 import NotFound from "./componant/NotFound/NotFound";
 
 
 
-let router= createBrowserRouter([
-{path:"",element:<Layout/>, children:[
-{index:true , element:<Home/>},
-{path:"blog" , element:<Blog/>},
-{path:"*" , element:<NotFound/>},
+let router = createBrowserRouter([
+  {
+    path: "", element: <Layout />, children: [
+      { index: true, element: <Home /> },
+      { path: "blog", element: <Blog /> },
+      { path: "*", element: <NotFound /> },
 
 
-] }
+    ]
+  }
 
 
 
- ],{
- 
- }) 
+], {
+  basename: '/assiment-14'
+})
 
 
 
 
 function App() {
-  
+
   return (
     <>
-     <RouterProvider router={router}></RouterProvider>
+      <RouterProvider router={router}></RouterProvider>
     </>
   )
 
